6751fce081e7809294295ffb61ba00a4c4b4ee09
VytenisSakinis <vytenis.sakinis@gmail.com>
2024-04-01 22:48:20 +0300

    ported issue tests from cypress to playwright

--- tests/issue.spec.ts
+++ tests/issue.spec.ts
@@ -1,8 +1,10 @@
 import { test, expect } from "@playwright/test";
 import { USER, URL } from "../test-data";
 import { before } from "node:test";
+import { faker } from '@faker-js/faker';
 
 test.describe("Issue creation", () => {
+  const userData = { randomName: faker.string.uuid, randomIssue: faker.string.uuid}
   test.beforeEach(async ({ page }) => {
     await page.goto("/register");
     await expect(page).toHaveTitle(/Web Client | WebIssues/);
@@ -12,14 +14,21 @@ test.describe("Issue creation", () => {
     await expect(page.getByText(/Log Out/i)).toBeVisible();
     await page.goto('/register/client/index.php?folder=1')
   });
-
   test("Should be able to create and delete issue", async ({page}) => {
     await page.click('[title="Add Issue"]')
-    await page.fill('#field-issues-issueName', "Playweight issue title test")
-    await page.fill('#field-issues-descriptionText', "Playweight issue title test describtion")
+    await page.fill('#field-issues-issueName', `${userData.randomName}`)
+    await page.fill('#field-issues-descriptionText', `${userData.randomIssue}`)
+    
+  })
+  test.afterAll(async ({page}) => {
+    await page.fill(`#field-search-searchBox`, `${userData.randomName}`);
+    await page.click("#field-search-searchSubmit");
+    await page.click(`[title="${userData.randomIssue}"]`)
+    await page.click('[title="Delete Issue"]')
     await page.click("#field-issues-okSubmit")
-    // await expect(page.isEnabled('//h2[text()="Hola"]'))
+    await page.click("#field-issues-okSubmit")
     await page.click('[title="Delete Issue"]')
     await page.click('#field-issues-okSubmit')
+    await expect(page.getByText(/Log Out/i)).toBeVisible();
   })
 });
