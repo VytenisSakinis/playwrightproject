a28952e0b7683589fbfa22ef29fb754224261447
VytenisSakinis <vytenis.sakinis@gmail.com>
2024-04-09 17:42:05 +0300

    screenshot tests

--- tests/sevenchar.spec.ts
+++ tests/sevenchar.spec.ts
@@ -45,4 +45,23 @@ test.describe('Test', () => {
         await page.$eval(selector, (element) => element.click());
         await expect(page.locator('#result')).toContainText('I t.e.s.t with a.t.t.i.t.u.d.e');
     });
+    test(`should be able to click and asser image`, async ({ page }) => {
+        await page.goto('https://eviltester.github.io/TestingApp/apps/testwith/version/1/testwith.html');
+        await page.fill('#w1lw0', 't');
+        await page.fill('#w1lw1', 'e');
+        await page.fill('#w1lw2', 's');
+        await page.fill('#w1lw3', 't');
+        await page.fill('#w2lw0', 'a');
+        await page.fill('#w2lw1', 't');
+        await page.fill('#w2lw2', 't');
+        await page.fill('#w2lw3', 'i');
+        await page.fill('#w2lw4', 't');
+        await page.fill('#w2lw5', 'u');
+        await page.fill('#w2lw6', 'd');
+        await page.fill('#w2lw7', 'e');
+        const selector = '//button[text()="Render"]';
+        await page.$eval(selector, (element) => element.click());
+        await page.locator('#canvas').screenshot();
+        await expect(page.locator('#canvas')).toHaveScreenshot();
+    });
 });
